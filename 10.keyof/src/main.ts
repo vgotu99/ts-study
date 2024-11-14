ab031d222b31d952ab80dfe40011a7a994ac5df2
vgotu99 <vgotu99@gmail.com>
2024-11-15 00:48:40 +0900

    modify typing error

--- 10.keyof/src/main.ts
+++ 10.keyof/src/main.ts
@@ -14,8 +14,8 @@ const user = {
   address: "seoul",
 };
 
-type UserKeys1 = keyof typeof user // keyof는 객체 타입에만 사용 가능하다.
-// typeof를 이용해서 user 객체를 {name: string, age: number, address: string} 형태의 객체 타입으로 변경하고 keyof를 이용해서 IUser의 key들만 모아 UserKeys1 type을 정의한다.
+type UserKeys1 = keyof typeof user // keyof는 타입에만 사용 가능하다.
+// typeof를 이용해서 user 객체를 {name: string; age: number; address: string} 형태의 객체 타입으로 변경하고 keyof를 이용해서 IUser의 key들만 모아 UserKeys1 type을 정의한다.
 
 enum UserRole {
   admin,
