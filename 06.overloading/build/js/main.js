9978c1b3428bf8df691d1e3fe1998653c514fd9b
vgotu99 <vgotu99@gmail.com>
2024-11-14 17:07:48 +0900

    apply command tsc -w

--- 06.overloading/build/js/main.js
+++ 06.overloading/build/js/main.js
@@ -1,20 +1,30 @@
 "use strict";
-const post = {
-    id: 1,
-    title: "post",
-    getLikeNumber(like) {
-        return like;
-    },
-};
-post.getLikeNumber(1);
-const post1 = {
-    id: 1,
-    title: "post1",
-};
-//key의 타입은 string / value의 타입은 string
-post1["description"] = "description1";
-//key의 타입은 string / value의 타입은 number
-post1["pages"] = 300;
-const userNames = ['John', 'Kim', 'Joe'];
-// key의 타입은 number / value의 타입은 string
-userNames[0] = 'Lee';
+// 함수 구현
+function add(a, b) {
+    return a + b;
+}
+add('hello', 'world');
+add(2, 3);
+// add(900, '원')  < number, string은 인자로 사용 불가
+function saySomething(word) {
+    if (typeof word === 'string') {
+        return word;
+    }
+    else if (Array.isArray(word)) {
+        return word.join(' ');
+    }
+    throw new Error('can not say undefined value');
+}
+saySomething('hello');
+saySomething(['hello', 'word']);
+function saySomething1(word) {
+    if (typeof word === 'string') {
+        return word;
+    }
+    else if (Array.isArray(word)) {
+        return word.join(' ');
+    }
+    throw new Error('can not say undefined value');
+}
+saySomething1('hello');
+saySomething1(['hello', 'word']);
